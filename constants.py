6f3cb08b711946a7077648573b7db5c6c762ee50
Nitesh Sawant <niteshsawant023@gmail.com>
2018-04-21 13:19:09 +0530

    Added pandas dataframe

---
+++ constants.py
@@ -0,0 +1,2 @@
+# url:https://developers.google.com/maps/documentation/geocoding/usage-limits
+GCA = 'AIzaSyAqfCUUO1iijQktx0_bxC3--qjUDOONDIo'
--- startup.py
+++ startup.py
@@ -1,7 +1,10 @@
 from requests import get
 from bs4 import BeautifulSoup
 from utility import *
+import googlemaps
+import pandas
 
+gmaps = googlemaps.Client(key='AIzaSyAqfCUUO1iijQktx0_bxC3--qjUDOONDIo')
 data_filename="data.csv"
 
 
@@ -12,17 +15,24 @@ def scarp_company_data():
 
     empty_file(data_filename)
     data_file = open(data_filename,'w+')
-    data_file.write("company_name,startup_goa_link\n")
+    data_file.write("company_name|startup_goa_link\n")
     for company in companies:
         link = company.find('a')
-        data_file.write(format_company_name(link.getText()) + ',' + link.get('href')+ '\n')
+        data_file.write(format_company_name(link.getText()) + '|' + link.get('href')+ '\n')
     data_file.close()
 
     print("data scrapped from startupgoa.org")    
 
 
+def gmaps_wrapper(address):
+    print(address)
+    address = address + ',goa'
+    return gmaps.geocode(address, region='IN')
+    
 def main():
     scarp_company_data()
+    df = df = pandas.read_csv('data.csv', delimiter='|')
+    df['gmap'] = df['company_name'].apply(gmaps_wrapper)
     pass
 
 if __name__ == '__main__':
