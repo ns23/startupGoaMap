3722515cee927b7690f6233728e9aa03462d8291
Nitesh Sawant <niteshsawant023@gmail.com>
2018-04-21 16:37:10 +0530

    version 0.1

--- startup.py
+++ startup.py
@@ -3,10 +3,14 @@ from bs4 import BeautifulSoup
 from utility import *
 import googlemaps
 import pandas
+import math
+from folium import Map, FeatureGroup, CircleMarker
+import math
+import webbrowser
 
-gmaps = googlemaps.Client(key='AIzaSyAqfCUUO1iijQktx0_bxC3--qjUDOONDIo')
+gmaps = googlemaps.Client(key='')# add your google map key here in order to script to work
 data_filename="data.csv"
-
+map_name = 'index.html'
 
 def scarp_company_data():
     htmlcontent = get("http://startupgoa.org/companies/#.WtoXUXVubZu")
@@ -25,15 +29,52 @@ def scarp_company_data():
 
 
 def gmaps_wrapper(address):
-    print(address)
     address = address + ',goa'
-    return gmaps.geocode(address, region='IN')
+    google_res = gmaps.geocode(address, region='IN')
     
-def main():
-    scarp_company_data()
-    df = df = pandas.read_csv('data.csv', delimiter='|')
+    if len(google_res) == 0:
+        return None
+    else:
+        return google_res    
+    
+def get_google_geo():
+    df = pandas.read_csv('data.csv', delimiter='|')
     df['gmap'] = df['company_name'].apply(gmaps_wrapper)
+
+    df["lat"] = df.gmap.apply(
+        lambda s: s[0]['geometry']['location']['lat'] if s != None else None)
+    df["lng"] = df.gmap.apply(
+        lambda s: s[0]['geometry']['location']['lng'] if s != None else None)
+    df["place_id"] = df.gmap.apply(
+        lambda s: s[0]['place_id'] if s != None else None)
+    df["formatted_address"] = df.gmap.apply(
+        lambda s: s[0]['formatted_address'] if s != None else None)
+    df.to_csv(path_or_buf='op.csv', sep='|')
+    return df   
+
+def compute_map(df):
+    lat = list(df['lat'])
+    lng = list(df['lng'])
+    company_name = list(df['company_name'])
+
+    map = Map(location=[15.2993, 74.1240], zoom_start=6, tiles="Mapbox Bright")
+    fgv = FeatureGroup()
+    for lt, ln, name in zip(lat, lng, company_name):
+        if not math.isnan(lt):
+            fgv.add_child(CircleMarker(location=[lt, ln], radius=6, popup=
+                name, fill_color="green", fill=True,  color='grey', fill_opacity=0.7))
     pass
+
+    map.add_child(fgv)
+    map.save(map_name)
+
+
+def main():
+    # scarp_company_data()
+    # df = get_google_geo()
+    df = pandas.read_csv('op.csv',delimiter='|')
+    compute_map(df)
+    webbrowser.open(map_name,new=2)
 
 if __name__ == '__main__':
     main()
